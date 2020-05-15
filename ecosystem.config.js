38be735f6ba6dc500665bca31dc3200cc6b0ea37
ShintaroItamoto <>
2020-05-15 15:51:49 +0900

    deploy config

---
+++ ecosystem.config.js
@@ -0,0 +1,33 @@
+const keyPath = process.env.SSH_PRIVATE_KEY_PATH || '~/.ssh/id_rsa';
+
+module.exports = {
+  apps: [
+    {
+      name: 'web-draw',
+      script: 'app.js',
+      args: 'start',
+      env: {
+        PORT: 3000,
+      },
+      env_production: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+
+  deploy: {
+    production: {
+      key: keyPath,
+      user: process.env.SERVER_USER,
+      host: [process.env.HOST],
+      ref: 'origin/master',
+      // repo: 'git@webdraw',
+      // repo: 'git@webdraw:itashin0501/online_draw_server.git',
+      repo: 'git@github.com:itashin0501/online_draw_server.git',
+      ssh_options: ['StrictHostKeyChecking=no', 'PasswordAuthentication=no'],
+      path: process.env.DEPLOY_TO,
+      'post-setup': 'npm install',
+      'post-deploy': 'pm2 restart ecosystem.config.js --env production',
+    },
+  },
+};
